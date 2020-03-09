e47bb7494ad8d8e2fdccb30335eed134a2424366
huzaifabaloch <huzbaudi3@gmail.com>
2020-03-09 22:38:01 +0500

    template inheriting <- code modification

--- basic.py
+++ basic.py
@@ -8,9 +8,12 @@ def index():
     name = 'The world make some noisy funny faces'
     mylist = list(name)
     mydict = {'name': 'flask-web-app'}
-    return render_template('basic.html', name=name, mylist=mylist,
+    return render_template('home.html', name=name, mylist=mylist,
                             mydict=mydict)
 
+@app.route('/puppy/<name>')
+def puppy_name(name):
+    return render_template('puppy.html', name=name)
 
 if __name__ == '__main__':
     app.run(debug=True)
